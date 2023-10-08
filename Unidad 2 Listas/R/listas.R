1bc699ee0b78b9e0cd23a4d6985163470b9339e1
Emiliano Jahir Espinoza Herrera <almno.nahum.espinozah@gmail.com>
2023-10-07 19:46:37 -0600

    listas R

---
+++ Unidad 2 Listas/R/listas.R
@@ -0,0 +1,7 @@
+ITIC3 <- list("Rainer Rivera", "Juan Prieto", "Santiago Herrera")
+Edades <- list(19,19,21)
+Carreras <- list("ITIC", "Logistica", "Gestion Empresarial", "Mecatronica", "Industrial")
+EdadesAlumnos <- list(ITIC3,Edades)
+View(EdadesAlumnos)
+ITIC3[1]
+View(Edades)
